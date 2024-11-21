70f9e92fe85e436a657edcdb8174bac6e7f3667e
CHAD STACHOWICZ <chadstachowicz@CHADs-MacBook-Air.local>
2024-11-21 14:04:52 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel
+from torchvision import datasets,transforms
+import torchvision
+import io
+from PIL import Image
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class TumorClassifier(nn.Module):
+    def __init__(self):
+        super(TumorClassifier, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 3)
+        self.conv2 = nn.Conv2d(6, 16, 3)
+        self.conv3 = nn.Conv2d(16, 64, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(4*56*56,120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.flatten(x)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+model = TumorClassifier()
+model.load_state_dict(torch.load("model/model-v1"))
+model.eval()
+
+transform = transforms.Compose([transforms.Resize(128),
+                                 transforms.CenterCrop(128),
+                                 transforms.ToTensor()])
+classes = ('glioma_tumor', 'meningioma_tumor', 'no_tumor', 'pituitary_tumor')
+
+# Define input data schema
+class InputData(BaseModel):
+    inputs: list
+
+# Initialize FastAPI app
+app = FastAPI()
+
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    # Read the uploaded image file
+    image_bytes = await file.read()
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+
+    # Apply transformations
+    transformed_image = transform(image).unsqueeze(0)  # Add batch dimension
+    with torch.no_grad():
+        outputs = model(transformed_image)
+        _, predicted = torch.max(outputs, 1)
+        print('Predicted: ',classes[predicted[0]])
+    return {"predictions": classes[predicted[0]]}
