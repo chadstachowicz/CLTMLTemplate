0eadacd700391e5253ff12d75914cc6efac331ac
CHAD STACHOWICZ <chadstachowicz@CHADs-MacBook-Air.local>
2024-11-24 11:22:00 -0500

    building

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse, JSONResponse
 from pydantic import BaseModel
@@ -8,6 +8,7 @@ import json
 import os
 import io
 from PIL import Image
+from pathlib import Path
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -87,7 +88,8 @@ class InputData(BaseModel):
 # Initialize FastAPI app
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
+# Directory where model JSON files are stored
+MODEL_DIRECTORY = "model"
 @app.get("/")
 def read_root():
     return FileResponse("static/index.html")
@@ -105,6 +107,35 @@ async def predict(file: UploadFile = File(...)):
         _, predicted = torch.max(outputs, 1)
         print('Predicted: ',classes[predicted[0]])
     return {"prediction": classes[predicted[0]], "description": tumor_descriptions[predicted[0]]["description"]}
+
+
+
+@app.get("/get-model")
+async def get_model(model_name: str):
+    """
+    Retrieve and parse the JSON file corresponding to the given model name.
+
+    Args:
+        model_name (str): The name of the model.
+
+    Returns:
+        JSON object: Parsed contents of the model JSON file.
+    """
+    # Construct the file path based on the model name
+    file_path = Path(MODEL_DIRECTORY) / f"{model_name}.json"
+    print(file_path)
+    # Check if the file exists
+    if not file_path.exists():
+        raise HTTPException(status_code=404, detail=f"Model file '{model_name}.json' not found.")
+
+    # Read and parse the JSON file
+    try:
+        with open(file_path, "r") as file:
+            model_data = json.load(file)
+    except json.JSONDecodeError:
+        raise HTTPException(status_code=400, detail="Error decoding JSON. Ensure the file is valid JSON.")
+
+    return {"model_name": model_name, "model_data": model_data}
 
 @app.get("/models")
 async def list_models():
